80e889463939a7e19d4a0a852af5f356a2db43e5
contejus <22385070+contejus@users.noreply.github.com>
2020-06-14 00:39:42 -0500

    remove dotenv and check for deploy

--- roulette.py
+++ roulette.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from discord.ext.commands.errors import CommandInvokeError
 from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
